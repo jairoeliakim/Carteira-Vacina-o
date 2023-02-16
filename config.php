c0cb84623d720281c562700b0eaa0957af7dadf1
Jairo Eliakim <56320488+jairoeliakim@users.noreply.github.com>
2023-02-16 20:56:50 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+//variaveis do banco
+$servidor = "localhost";
+$usuario = "root";
+$senha = "vertrigo";
+$bd = "bd+vacinas";
+$url = "https://localhost/carteiradevacina";
+
+//VARIAVEIS GLOBAIS
+$email = "jairo.costa@estudante.iftm.edu.br";
+$endereço = "Av. Liria Terezinha Amaral - Dona Diva";
+$cep = "38740-000";
+$cel = "(34) 3131-3131";
+$cel_link = "tel: 034 31313131";
+
+$nome_empresa= "Carteira de Vacinas";
+
+
+//DISPAROS AUTOMATIZADOS DE EMAIL
+$enviar_total_email ="";
+
+?>
